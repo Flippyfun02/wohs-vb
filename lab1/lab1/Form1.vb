d54a2db59d2844bb7c12e2067ac14aae0838d9c8
Flippyfun02 <86199844+Flippyfun02@users.noreply.github.com>
2022-01-26 12:43:45 -0500

    Add files via upload

---
+++ lab1/lab1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    'This is my first program'
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Hello World!")
+    End Sub
+End Class
