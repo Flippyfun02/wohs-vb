dd3721d9671bf6c88cfb04094df164a206b4f6da
Flippyfun02 <86199844+Flippyfun02@users.noreply.github.com>
2022-01-26 12:45:12 -0500

    Add files via upload

---
+++ lab2/lab2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        Label1.Text = "Run Time"
+        Label1.BackColor = Color.Blue
+        Me.BackColor = Color.Green
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = "Load Time"
+        Label1.BackColor = Color.Red
+        Me.BackColor = Color.Purple
+    End Sub
+End Class
