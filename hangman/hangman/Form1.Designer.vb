1a6cde8f41cbf4ed9798337e98badfdeebc1218f
Flippyfun02 <86199844+Flippyfun02@users.noreply.github.com>
2022-01-26 12:40:57 -0500

    Add files via upload

---
+++ hangman/hangman/Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.pnlLetters = New System.Windows.Forms.Panel
+        Me.txtLetter10 = New System.Windows.Forms.TextBox
+        Me.txtLetter9 = New System.Windows.Forms.TextBox
+        Me.txtLetter8 = New System.Windows.Forms.TextBox
+        Me.txtLetter5 = New System.Windows.Forms.TextBox
+        Me.txtLetter7 = New System.Windows.Forms.TextBox
+        Me.txtLetter6 = New System.Windows.Forms.TextBox
+        Me.txtLetter4 = New System.Windows.Forms.TextBox
+        Me.txtLetter3 = New System.Windows.Forms.TextBox
+        Me.txtLetter2 = New System.Windows.Forms.TextBox
+        Me.txtLetter1 = New System.Windows.Forms.TextBox
+        Me.pbcHangmanBmp = New System.Windows.Forms.PictureBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtGuesses = New System.Windows.Forms.TextBox
+        Me.txtMisses = New System.Windows.Forms.TextBox
+        Me.txtGamesLost = New System.Windows.Forms.TextBox
+        Me.txtGamesWon = New System.Windows.Forms.TextBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtInput = New System.Windows.Forms.TextBox
+        Me.btnSubmit = New System.Windows.Forms.Button
+        Me.imlHangmanBmps = New System.Windows.Forms.ImageList(Me.components)
+        Me.pnlLetters.SuspendLayout()
+        CType(Me.pbcHangmanBmp, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlLetters
+        '
+        Me.pnlLetters.Controls.Add(Me.txtLetter10)
+        Me.pnlLetters.Controls.Add(Me.txtLetter9)
+        Me.pnlLetters.Controls.Add(Me.txtLetter8)
+        Me.pnlLetters.Controls.Add(Me.txtLetter5)
+        Me.pnlLetters.Controls.Add(Me.txtLetter7)
+        Me.pnlLetters.Controls.Add(Me.txtLetter6)
+        Me.pnlLetters.Controls.Add(Me.txtLetter4)
+        Me.pnlLetters.Controls.Add(Me.txtLetter3)
+        Me.pnlLetters.Controls.Add(Me.txtLetter2)
+        Me.pnlLetters.Controls.Add(Me.txtLetter1)
+        Me.pnlLetters.Location = New System.Drawing.Point(12, 12)
+        Me.pnlLetters.Name = "pnlLetters"
+        Me.pnlLetters.Size = New System.Drawing.Size(321, 38)
+        Me.pnlLetters.TabIndex = 0
+        '
+        'txtLetter10
+        '
+        Me.txtLetter10.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter10.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter10.Location = New System.Drawing.Point(291, 3)
+        Me.txtLetter10.Multiline = True
+        Me.txtLetter10.Name = "txtLetter10"
+        Me.txtLetter10.ReadOnly = True
+        Me.txtLetter10.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter10.TabIndex = 9
+        '
+        'txtLetter9
+        '
+        Me.txtLetter9.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter9.Location = New System.Drawing.Point(259, 3)
+        Me.txtLetter9.Multiline = True
+        Me.txtLetter9.Name = "txtLetter9"
+        Me.txtLetter9.ReadOnly = True
+        Me.txtLetter9.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter9.TabIndex = 8
+        '
+        'txtLetter8
+        '
+        Me.txtLetter8.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter8.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter8.Location = New System.Drawing.Point(227, 3)
+        Me.txtLetter8.Multiline = True
+        Me.txtLetter8.Name = "txtLetter8"
+        Me.txtLetter8.ReadOnly = True
+        Me.txtLetter8.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter8.TabIndex = 7
+        '
+        'txtLetter5
+        '
+        Me.txtLetter5.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter5.Location = New System.Drawing.Point(131, 3)
+        Me.txtLetter5.Multiline = True
+        Me.txtLetter5.Name = "txtLetter5"
+        Me.txtLetter5.ReadOnly = True
+        Me.txtLetter5.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter5.TabIndex = 6
+        '
+        'txtLetter7
+        '
+        Me.txtLetter7.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter7.Location = New System.Drawing.Point(195, 3)
+        Me.txtLetter7.Multiline = True
+        Me.txtLetter7.Name = "txtLetter7"
+        Me.txtLetter7.ReadOnly = True
+        Me.txtLetter7.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter7.TabIndex = 4
+        '
+        'txtLetter6
+        '
+        Me.txtLetter6.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter6.Location = New System.Drawing.Point(163, 3)
+        Me.txtLetter6.Multiline = True
+        Me.txtLetter6.Name = "txtLetter6"
+        Me.txtLetter6.ReadOnly = True
+        Me.txtLetter6.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter6.TabIndex = 5
+        '
+        'txtLetter4
+        '
+        Me.txtLetter4.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter4.Location = New System.Drawing.Point(99, 3)
+        Me.txtLetter4.Multiline = True
+        Me.txtLetter4.Name = "txtLetter4"
+        Me.txtLetter4.ReadOnly = True
+        Me.txtLetter4.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter4.TabIndex = 3
+        '
+        'txtLetter3
+        '
+        Me.txtLetter3.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter3.Location = New System.Drawing.Point(67, 3)
+        Me.txtLetter3.Multiline = True
+        Me.txtLetter3.Name = "txtLetter3"
+        Me.txtLetter3.ReadOnly = True
+        Me.txtLetter3.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter3.TabIndex = 2
+        '
+        'txtLetter2
+        '
+        Me.txtLetter2.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter2.Location = New System.Drawing.Point(35, 3)
+        Me.txtLetter2.Multiline = True
+        Me.txtLetter2.Name = "txtLetter2"
+        Me.txtLetter2.ReadOnly = True
+        Me.txtLetter2.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter2.TabIndex = 1
+        '
+        'txtLetter1
+        '
+        Me.txtLetter1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtLetter1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetter1.Location = New System.Drawing.Point(3, 3)
+        Me.txtLetter1.Multiline = True
+        Me.txtLetter1.Name = "txtLetter1"
+        Me.txtLetter1.ReadOnly = True
+        Me.txtLetter1.Size = New System.Drawing.Size(26, 32)
+        Me.txtLetter1.TabIndex = 0
+        '
+        'pbcHangmanBmp
+        '
+        Me.pbcHangmanBmp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.pbcHangmanBmp.Location = New System.Drawing.Point(339, 12)
+        Me.pbcHangmanBmp.Name = "pbcHangmanBmp"
+        Me.pbcHangmanBmp.Size = New System.Drawing.Size(102, 102)
+        Me.pbcHangmanBmp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbcHangmanBmp.TabIndex = 1
+        Me.pbcHangmanBmp.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtGuesses)
+        Me.GroupBox1.Controls.Add(Me.txtMisses)
+        Me.GroupBox1.Controls.Add(Me.txtGamesLost)
+        Me.GroupBox1.Controls.Add(Me.txtGamesWon)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 120)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(426, 78)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Game Stats"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(277, 48)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "No. Games Lost:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(277, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "No. Games Won:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Letters Guessed:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "No. Of Misses:"
+        '
+        'txtGuesses
+        '
+        Me.txtGuesses.Location = New System.Drawing.Point(110, 45)
+        Me.txtGuesses.Name = "txtGuesses"
+        Me.txtGuesses.Size = New System.Drawing.Size(144, 20)
+        Me.txtGuesses.TabIndex = 6
+        '
+        'txtMisses
+        '
+        Me.txtMisses.Location = New System.Drawing.Point(110, 19)
+        Me.txtMisses.Name = "txtMisses"
+        Me.txtMisses.Size = New System.Drawing.Size(48, 20)
+        Me.txtMisses.TabIndex = 5
+        '
+        'txtGamesLost
+        '
+        Me.txtGamesLost.Location = New System.Drawing.Point(372, 45)
+        Me.txtGamesLost.Name = "txtGamesLost"
+        Me.txtGamesLost.Size = New System.Drawing.Size(48, 20)
+        Me.txtGamesLost.TabIndex = 3
+        '
+        'txtGamesWon
+        '
+        Me.txtGamesWon.Location = New System.Drawing.Point(372, 19)
+        Me.txtGamesWon.Name = "txtGamesWon"
+        Me.txtGamesWon.Size = New System.Drawing.Size(48, 20)
+        Me.txtGamesWon.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 76)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(107, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Enter a Letter Guess:"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(125, 73)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(108, 20)
+        Me.txtInput.TabIndex = 4
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(239, 73)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 5
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'imlHangmanBmps
+        '
+        Me.imlHangmanBmps.ImageStream = CType(resources.GetObject("imlHangmanBmps.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imlHangmanBmps.TransparentColor = System.Drawing.Color.Transparent
+        Me.imlHangmanBmps.Images.SetKeyName(0, "100.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(1, "80.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(2, "60.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(3, "40.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(4, "20.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(5, "10.bmp")
+        Me.imlHangmanBmps.Images.SetKeyName(6, "0.bmp")
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 210)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.pbcHangmanBmp)
+        Me.Controls.Add(Me.pnlLetters)
+        Me.Name = "frmMain"
+        Me.Text = "Hangman"
+        Me.pnlLetters.ResumeLayout(False)
+        Me.pnlLetters.PerformLayout()
+        CType(Me.pbcHangmanBmp, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pnlLetters As System.Windows.Forms.Panel
+    Friend WithEvents txtLetter10 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter9 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter8 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter5 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter7 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter6 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtLetter1 As System.Windows.Forms.TextBox
+    Friend WithEvents pbcHangmanBmp As System.Windows.Forms.PictureBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtMisses As System.Windows.Forms.TextBox
+    Friend WithEvents txtGamesLost As System.Windows.Forms.TextBox
+    Friend WithEvents txtGamesWon As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtGuesses As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents imlHangmanBmps As System.Windows.Forms.ImageList
+
+End Class
---
+++ hangman/hangman/Form1.vb
@@ -0,0 +1,217 @@
+﻿Public Class frmMain
+    Const cTitleBarMsg As String = "HANGMAN" ' Titlebar message
+    Private strWordListArray(25) As String ' list of game words
+    Private strGameWord As String = "" ' stores word currently in play
+    Private strWrong As String = "" ' Stores a list of missed guesses
+    Private intHangmanNo As Integer = 6 ' controls hangman display
+
+    ' stats
+    Private intMisses As Integer = 0
+    Private intNoRight As Integer = 0
+    Private intPlayAgain As Integer = 0
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadWordArray()
+        InitializeGameStats()
+        StartTheGame()
+    End Sub
+
+    Sub LoadWordArray()
+        strWordListArray(0) = "ELEPHANT"
+        strWordListArray(1) = "CARRIAGE"
+        strWordListArray(2) = "ENVELOPE"
+        strWordListArray(3) = "PRESIDENT"
+        strWordListArray(4) = "ELLIPTICAL"
+        strWordListArray(5) = "MARKER"
+        strWordListArray(6) = "BATTLESHIP"
+        strWordListArray(7) = "TERMINATE"
+        strWordListArray(8) = "REJOICE"
+        strWordListArray(9) = "LIBERTY"
+        strWordListArray(10) = "CLASSIC"
+        strWordListArray(11) = "REFLEX"
+        strWordListArray(12) = "AIRPLANE"
+        strWordListArray(13) = "DWELLING"
+        strWordListArray(14) = "APARTMENT"
+        strWordListArray(15) = "RENTAL"
+        strWordListArray(16) = "FAUCET"
+        strWordListArray(17) = "BEDROOM"
+        strWordListArray(18) = "VOLLEY"
+        strWordListArray(19) = "WHISTLE"
+        strWordListArray(20) = "PENCIL"
+        strWordListArray(21) = "THIEF"
+        strWordListArray(22) = "DRAGON"
+        strWordListArray(23) = "POTATO"
+        strWordListArray(24) = "VOYAGER"
+        strWordListArray(25) = "HANGMAN"
+    End Sub
+
+    Sub InitializeGameStats()
+        txtGamesWon.Text = 0
+        txtGamesLost.Text = 0
+    End Sub
+
+    Sub StartTheGame()
+        ResetDefaultSettings()
+        strGameWord = RetrieveWord()
+        FormatGameWord()
+        txtInput.Focus()
+    End Sub
+    Sub ResetDefaultSettings()
+        intMisses = 0
+        intNoRight = 0
+        txtGuesses.Text = ""
+        txtMisses.Text = intMisses
+        txtInput.Text = ""
+        intHangmanNo = 6
+        pbcHangmanBmp.Image = imlHangmanBmps.Images.Item(6)
+    End Sub
+    Function RetrieveWord() As String
+        Dim intRandomNo As Integer = 0
+        Dim r As New Random
+
+        intRandomNo = r.Next(strWordListArray.Length)
+        Return strWordListArray(intRandomNo)
+    End Function
+    Sub FormatGameWord()
+        Dim intCounter As Integer = 1
+
+        ' blank words
+        txtLetter1.Text = ""
+        txtLetter2.Text = ""
+        txtLetter3.Text = ""
+        txtLetter4.Text = ""
+        txtLetter5.Text = ""
+        txtLetter6.Text = ""
+        txtLetter7.Text = ""
+        txtLetter8.Text = ""
+        txtLetter9.Text = ""
+        txtLetter10.Text = ""
+
+        For intCounter = 1 To strGameWord.Length
+            If intCounter = 1 Then txtLetter1.Text = "-"
+            If intCounter = 2 Then txtLetter2.Text = "-"
+            If intCounter = 3 Then txtLetter3.Text = "-"
+            If intCounter = 4 Then txtLetter4.Text = "-"
+            If intCounter = 5 Then txtLetter5.Text = "-"
+            If intCounter = 6 Then txtLetter6.Text = "-"
+            If intCounter = 7 Then txtLetter7.Text = "-"
+            If intCounter = 8 Then txtLetter8.Text = "-"
+            If intCounter = 9 Then txtLetter9.Text = "-"
+            If intCounter = 10 Then txtLetter10.Text = "-"
+        Next
+    End Sub
+
+    Private Sub btnSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        Dim intCounter As Integer = 0
+        Dim strWordLetter As String = ""
+        Dim strInputOk As String = ""
+
+        strInputOk = ValidatePlayerInput()
+        If strInputOk = "Error" Then
+            Exit Sub
+        End If
+
+        ' if user already guessed letter
+        If Strings.InStr(1, txtGuesses.Text, txtInput.Text.ToUpper, 1) <> 0 Then
+            MessageBox.Show("Error: Letter has already been guessed.")
+        Else
+            txtGuesses.Text += " " + txtInput.Text
+
+            ' if guess is wrong
+            If Strings.InStr(1, strGameWord.ToUpper, txtInput.Text.ToUpper, 1) = 0 Then
+                strWrong += " " + txtInput.Text.ToUpper
+                intMisses += 1
+                txtMisses.Text = intMisses
+
+                ' update hangman
+                intHangmanNo -= 1
+                pbcHangmanBmp.Image = imlHangmanBmps.Images.Item(intHangmanNo)
+
+                ' losing game
+                If intMisses = 6 Then
+                    txtGamesLost.Text += 1
+                    PromptForNewGame("Sorry. You have lost. The word was " + strGameWord + ".")
+                End If
+            End If
+
+            ' go through each letter
+            For intCounter = 1 To strGameWord.Length
+                ' select one letter at a time
+                strWordLetter = Mid(strGameWord, intCounter, 1)
+                ' if letter matches player's guess
+                If strWordLetter.ToUpper = txtInput.Text.ToUpper Then
+                    intNoRight += 1
+                    FlipGameLetter(intCounter)
+                End If
+            Next
+        End If
+
+        If intNoRight = strGameWord.Length Then
+            txtGamesWon.Text += 1
+            PromptForNewGame("Congratulations. You have won!")
+        End If
+
+        txtInput.Text = ""
+        txtInput.Focus()
+
+    End Sub
+    Function ValidatePlayerInput() As String
+        If txtInput.Text = "" Then
+            MessageBox.Show("Error: You must enter a letter.")
+            txtInput.Text = ""
+            txtInput.Focus()
+            Return "Error"
+        End If
+
+        If txtInput.Text.Length > 1 Then
+            MessageBox.Show("Error: You may only enter one letter per guess")
+            txtInput.Text = ""
+            txtInput.Focus()
+            Return "Error"
+        End If
+        If IsNumeric(txtInput.Text) Then
+            MessageBox.Show("Error: Numeric entries are not valid.")
+            txtInput.Text = ""
+            txtInput.Focus()
+            Return "Error"
+        End If
+        Return "Passed"
+
+    End Function
+    Sub FlipGameLetter(ByVal intLetterNumber As Integer)
+        ' display letter
+        Select Case intLetterNumber
+            Case 1
+                txtLetter1.Text = txtInput.Text.ToUpper
+            Case 2
+                txtLetter2.Text = txtInput.Text.ToUpper
+            Case 3
+                txtLetter3.Text = txtInput.Text.ToUpper
+            Case 4
+                txtLetter4.Text = txtInput.Text.ToUpper
+            Case 5
+                txtLetter5.Text = txtInput.Text.ToUpper
+            Case 6
+                txtLetter6.Text = txtInput.Text.ToUpper
+            Case 7
+                txtLetter7.Text = txtInput.Text.ToUpper
+            Case 8
+                txtLetter8.Text = txtInput.Text.ToUpper
+            Case 9
+                txtLetter9.Text = txtInput.Text.ToUpper
+            Case 10
+                txtLetter10.Text = txtInput.Text.ToUpper
+        End Select
+    End Sub
+
+    Sub PromptForNewGame(ByVal strMessage As String)
+        intPlayAgain = MessageBox.Show(strMessage & " Would you like to play again?", _
+                                       cTitleBarMsg, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If intPlayAgain = 6 Then
+            StartTheGame()
+        Else
+            Application.Exit()
+        End If
+    End Sub
+
+End Class
